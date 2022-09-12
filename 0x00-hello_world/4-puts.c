9299693946bbd48f15c3222376027900ce3fe5ff
hauwa ibrahim <hauwaibrahim585@gmail.com>
2022-09-12 05:42:35 -0700

    Put function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
-gcc $CFILE -o cisfun
 
-int main (void)
+/**
+ * main - Print string to a newline using puts function
+ *
+ * Description: Using puts function to print string in a newline
+ *
+ * Return: 1 if error occurs. 0 otherwise
+ */
+
+int main(void)
 {
-
-	printf("Programming is like building a multilingual puzzle/n");
-
-		return (0);
-
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
+
