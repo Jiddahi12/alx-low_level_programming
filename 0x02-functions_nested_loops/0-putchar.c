b9614536f8388873046b6855c2deba5ca3b00399
hauwa ibrahim <hauwaibrahim585@gmail.com>
2022-09-19 17:27:25 -0700

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,9 @@
 #include "main.h"
 
 /**
- * main-main block
- * Description:print -putchar in a new line
- * Return:this program returns zero
+ * main - prints putchar
+ * Return: Always 0
  */
-
 int main(void)
 {
 	_putchar('_');
