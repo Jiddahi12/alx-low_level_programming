84f7bbdf47ba20dd234976df8df8d415bbfa76c3
hauwa ibrahim <hauwaibrahim585@gmail.com>
2022-10-25 18:16:47 -0700

    linking

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * print_listint - Print all the list of a link while counting the node
+ * @h: the list with a type list_t
+ * Return: retunrn the total nodee
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	const listint_t *tmp;
+	size_t count;
+
+	count = 0;
+	for (tmp = h; tmp != NULL; tmp = tmp->next)
+	{
+		count++;
+		printf("%d\n", tmp->n);
+	}
+	return (count);
+}
